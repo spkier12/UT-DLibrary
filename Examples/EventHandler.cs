492c1612fbaff9b63f47145144700b9bcba82c75
Klump <23707746+sompklump@users.noreply.github.com>
2021-05-31 23:23:22 -0700

    Add files via upload

---
+++ Examples/EventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uech_Discord_Library
+{
+    public class Test
+    {
+        public void Start()
+        {
+            Listeners listeners = new Listeners();
+            listeners.OnResponse += new EventHandler<DiscordEventArgs>(onResponse);
+        }
+        private void onResponse(object sender, DiscordEventArgs e)
+        {
+
+        }
+    }
+}
